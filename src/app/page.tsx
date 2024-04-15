b0c5a8abb0048af024bf7aed0725f04351016f13
Duong Nguyen <duong.nguyen@holdstation.com>
2024-04-15 18:38:40 +0700

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-      <h1>Helllo teest ga</h1>
+    <main className="h-[100vh] bg-red-300">
+      <h1>hello</h1>
     </main>
   );
 }
